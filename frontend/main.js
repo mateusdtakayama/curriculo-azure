75fc9f67c5a814478ee7496eddd45b276ff9271e
mateusdtakayama <mateus25delai@gmail.com>
2024-01-27 21:46:13 -0300

    feat: add azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,19 @@
- const increaseCounter = () => {
-    let currentCount = parseInt(document.getElementById("counter").innerText);
-    
-    let count = 30
-    
-    count++
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
 
-    document.getElementById("counter").innerText = count;
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count =  response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
 }
-window.addEventListener('DOMContentLoaded', (event) => {
-    increaseCounter();
-});
